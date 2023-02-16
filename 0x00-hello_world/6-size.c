beb524c7aeed933d5661ff14328a1ffafa76a9df
Akande Lateef <akandelateef0@gmail.com>
2023-02-16 15:09:08 -0800

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,8 +16,8 @@ int main(void)
 
 	printf("size of a char: %zu byte(s)\n", sizeof(charSize));
 	printf("size of an int: %zu byte(s)\n", sizeof(intSize));
-	printf("size of a long int: %zu byte(s)\n", long() sizeof(intSize));
-	printf("size of a long long int: %zu byte(s)\n", long() long() sizeof(intSize));
+	printf("size of a long int: %zu byte(s)\n", (long) sizeof(intSize));
+	printf("size of a long long int: %zu byte(s)\n", (long long) sizeof(intSize));
 	printf("size of a float: %zu byte(s)\n", sizeof(floatSize));
 
 	return (0);
