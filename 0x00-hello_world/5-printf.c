aa18d4c40e867e21225ba4badf42ad55269d0830
Akande Lateef <akandelateef0@gmail.com>
2023-02-16 14:25:24 -0800

    printf is peaceful

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program to print
+ * with proper grammar, but the outcome is a piece of art,
+ * using printf function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
