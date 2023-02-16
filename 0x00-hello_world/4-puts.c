880e895f6f708f70ea0aa752fb76a9c5e1968464
Akande Lateef <akandelateef0@gmail.com>
2023-02-16 14:08:41 -0800

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: write a program file that will print 
+ * Description: write a program file that will print
  * exactly "Programming is like building a multilingual puzzle"
- *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-	}
+}
