19501f41746e8122472a78c3dd67bc6269f1f0c8
Akande Lateef <akandelateef0@gmail.com>
2023-02-17 13:44:16 -0800

    print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program to print
+ * the alphabets using putchar function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for (i = 'a'; i >= 'z'; ch++)
+		putchar(ch);
+	return (0);
+
