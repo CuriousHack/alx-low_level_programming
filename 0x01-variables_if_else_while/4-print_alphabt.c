b29a2bba2a5752d9a8d53ad772b1980c0b3d9147
Akande Lateef <akandelateef0@gmail.com>
2023-02-17 14:38:57 -0800

    e and q out

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a program to print
+ * the alphabets without eand q 
+ * using putchar function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == e || ch ==q)
+		{
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
