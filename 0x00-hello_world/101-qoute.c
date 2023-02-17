e36b8376fee7ba845c6138567eabc294397f9410
Akande Lateef <akandelateef0@gmail.com>
2023-02-16 16:20:39 -0800

    Qoute achieved

---
+++ 0x00-hello_world/101-qoute.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a c program to print without printf nd puts
+ *
+ * Return: Always 0 (Success)
+ *
+ **/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+return (0);
+
+}
