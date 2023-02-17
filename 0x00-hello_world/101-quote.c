f817b9f7ea87f0d3e54b22106d9faae07b2d6399
Akande Lateef <akandelateef0@gmail.com>
2023-02-16 16:34:36 -0800

    new libranry added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: a c program to print without printf nd puts
- *
- * Return: Always 0 (Success)
- *
+ * Description:print without printf and puts
+ * Return: Always 1 (Success)
  **/
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-return (0);
+return (1);
 
 }
