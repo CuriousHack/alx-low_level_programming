7c6aaeb3ff734f93e5a6918717d9b2fa82ac65d3
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 14:11:53 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - check the code.
+ * @int: param type
+ * Return: Always 0.
+ */
+int _abs(int)
+{
+	int i;
+	int j;
+
+	if (i < 0)
+	{
+		j = i * i;
+	}
+	else
+	{
+		j = i;
+	}
+	return (j);
+}
