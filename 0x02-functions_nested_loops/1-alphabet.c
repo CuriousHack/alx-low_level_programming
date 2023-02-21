a7b62104e5f60737ec4ed8ea7442e6a11c15742b
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 06:11:31 -0800

    updated

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -18,9 +18,9 @@ int main(void)
  */
 void print_alphabet(void)
 {
-	char ch;
+	char c;
 
-	for (ch = 'a'; ch >= 'z'; ch++)
-	_putchar(&ch);
+	for (c = 'a'; c >= 'z'; c++)
+	_putchar(c);
 	_putchar('\n');
 }
