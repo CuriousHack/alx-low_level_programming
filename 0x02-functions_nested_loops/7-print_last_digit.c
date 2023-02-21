ffb6818057d36d3acf4b5db90490f672c5b4d44c
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 14:28:54 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code.
+ *@i: test parameter
+ * Return: Always 0.
+ */
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	return (j);
+}
