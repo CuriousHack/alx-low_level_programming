2707569ccc8e4c264614bf3b2e5fe976bd2fe373
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 13:01:40 -0800

    Check lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - function to
+ * check if a char is a
+ * lowercase
+ *
+ * @c: test variable
+ * Return: return 1 if true
+ **/
+
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (c == i)
+			return (1);
+		return (0);
+}
