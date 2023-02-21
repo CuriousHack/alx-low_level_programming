f87170788a98fe3ee3ba4ae2a3a8e7866dd8b404
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 06:49:31 -0800

    x10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet_x10 - toprint  alphabet
+ * in lower case 10x
+ *
+ **/
+void print_alphabet_x10(void)
+{
+	char ch;
+	char i = 0;
+
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		putchar('\n');
+		i++;
+	}
+}
