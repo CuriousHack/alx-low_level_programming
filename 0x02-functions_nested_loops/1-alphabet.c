04a113f2b08a37c90b771d36ed303efc58c3bbb3
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 06:35:37 -0800

    new update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,26 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Description: todisplay alphabets
- *
- * Return: Always 0.
- */
-
-int main(void)
+* print_alphabet - to display alphabet
+*/
+void print_alphabet(void)
 {
-	/**
-	* print_alphabet - to display alphabet
-	*/
-	void print_alphabet(void)
-	{
-		char c;
+	char c;
 
-		for (c = 'a'; c <= 'z'; c++)
-		_putchar(c);
-		_putchar('\n');
-	}
-	print_alphabet();
-	return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+_putchar('\n');
 }
