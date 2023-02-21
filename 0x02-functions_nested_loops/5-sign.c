bfa6cf664f7acef4f310273cb2770845c1133113
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 13:57:29 -0800

    check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ *@n: test parameter
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		i = 1;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		i = -1;
+	}
+	else
+	{
+		-putchar('0');
+		i = 0;
+	}
+	return (i);
