6cea05285a8686b2b8da226f101eef54c6072f22
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 16:47:33 -0800

    added add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code.
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = (a + b);
+	return (c);
+}
