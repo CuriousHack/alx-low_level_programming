1b436acee4d6a2386b0bf752cb5dfbbeddd87403
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 16:01:33 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - check the code.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a;
+	int i;
+	int b = 0;
+	int c = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		while (b < 9)
+		{
+			while (c < 9)
+			{
+				i = b * c;
+				_putchar(i);
+				_putchar(',');
+				_putchar(' ');
+				c++;
+			}
+			_putchar('\n');
+			b++;
+		}
+	}
+	return (0);
+}
