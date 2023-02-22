8fe296ed3d591c6e49b7f8d0c3786ef02ed03ae7
Akande Lateef <akandelateef0@gmail.com>
2023-02-21 17:42:26 -0800

    updated

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,6 +15,7 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
+
 			if (n == 98)
 				continue;
 			printf(", ");
@@ -26,6 +27,7 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
+
 			if (n == 98)
 				continue;
 			printf(", ");
