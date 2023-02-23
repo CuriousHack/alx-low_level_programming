83b85928ea34dc3f22e853b15c0fe51a32f6df00
Akande Lateef <akandelateef0@gmail.com>
2023-02-23 10:07:12 -0800

    multiples

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply values
+ * @a: First parameter
+ * @b: Second parameter
+ *
+ * Return: c as multiple
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
