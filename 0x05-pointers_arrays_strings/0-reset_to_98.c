f860d7bf43b540f4f6baf6863d476e9624f661a1
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 08:36:04 -0800

    initialization and declaration

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset a number to 98
+ * @n: variable to change
+ * Return: nothing
+ *
+ */
+int reset_to_98(int *n)
+{
+	int *p;
+
+	*p = &n
+	p = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains the file for working with pointers, arrays and strings.
