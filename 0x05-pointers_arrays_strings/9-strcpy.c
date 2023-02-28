419690fc5748787789e9da78b920e7ac3fc3840a
Akande Lateef <akandelateef0@gmail.com>
2023-02-28 12:00:12 -0800

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - function to copy string
+ * @dest: destination to copy to
+ * @src: sourcé to copy from
+ * Return: return dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
