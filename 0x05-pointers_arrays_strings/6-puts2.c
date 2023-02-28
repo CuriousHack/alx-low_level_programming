2b7c6693797a7ae7962e7330de62b5fdb5531799
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 18:51:32 -0800

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print every other character
+ * @str: string tosplit
+ * Return: no return
+ */
+void puts2(char *str)
+{
+	int count = 0' i;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	for (i = 0; i <= count; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
