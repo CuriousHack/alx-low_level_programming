4fad2de7f039b44f90b37cd61906dda750f2857c
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 16:10:24 -0800

    reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#import "main.h"
+
+/**
+ * print_rev - function to reverse a string
+ * @s: string tobe reversed
+ * Return: nothing toreturn
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+	{
+		count++;
+	}
+	for (; count >= 0; count--)
+	{
+		_putchar(count);
+	}
+	_putchar('\n');
+}
