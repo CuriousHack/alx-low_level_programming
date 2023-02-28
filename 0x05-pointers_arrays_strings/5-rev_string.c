73decd1bd55b8a7f84ef448850b44d6e1f6ec781
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 17:59:27 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,8 +16,8 @@ void rev_string(char *s)
 	}
 	for (i = 0; i <= count / 2; i++)
 	{
-		temp = s[i];
-		s[i] = s[count - i - 1];
-		s[count - i - 1] = temp;
+		temp = *s[i];
+		*s[i] = *s[count - i - 1];
+		*s[count - i - 1] = temp;
 	}
 }
