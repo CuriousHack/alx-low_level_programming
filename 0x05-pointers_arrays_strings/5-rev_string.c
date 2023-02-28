a46e5338c69f16e20ba62696f664c75b17122e86
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 17:44:20 -0800

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,17 @@
  */
 void rev_string(char *s)
 {
-	int count;
+	int count, i;
+	char temp;
 
 	while (s[count] != '\0')
 	{
 		count++;
 	}
-	for (int i = 0; i <= count / 2; i--)
+	for (i = 0; i <= count / 2; i--)
 	{
-		char temp = s[i];
+		temp = s[i];
 		s[i] = s[count - i - 1];
-		s[count - i - 1] = s[i];
+		s[count - i - 1] = temp;
 	}
 }
