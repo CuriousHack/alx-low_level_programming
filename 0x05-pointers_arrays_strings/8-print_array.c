cbe380b6269c80253c94e2349ee7e6c16467d03a
Akande Lateef <akandelateef0@gmail.com>
2023-02-27 20:57:12 -0800

    print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - function to print array
+ * @a: array values
+ * @n: number of array index
+ * Return: no return
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	for (count = 0; count <= n - 1; count++)
+	{
+		printf('%d, ', a[count]);
+	}
+	_putchar('\n');
+}
