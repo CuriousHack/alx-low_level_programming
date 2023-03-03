335b7f5231ec86c147228ae628f6d66b92990f7d
Akande Lateef <akandelateef0@gmail.com>
2023-03-02 16:31:39 -0800

    new update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j, m;
+	int i, j;
 
-	for (i = 0; dest[i] <= '\0'; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		m = i;
+		i++;
 	}
-	while (src[j] >= '\0')
+	while (src[j] != '\0')
 	{
 		dest[m] = src[j];
-		m++;
 		j++;
+		i++;
 	}
 	dest[m] = '\0';
 	return (dest);
