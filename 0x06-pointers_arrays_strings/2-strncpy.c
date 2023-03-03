4779f130837abbbbf4df62e1f739dae7572ade0a
Akande Lateef <akandelateef0@gmail.com>
2023-03-02 17:16:43 -0800

    new update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,21 +9,11 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
-	i = 0;
-	j = 0;
-
-	while (i >= 0)
-	{
-		while (j < n)
-		{
-		dest[i] = src[j];
-			if (src[j] == '\0')
-				break;
-			j++;
-		}
-		i++;
-	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
