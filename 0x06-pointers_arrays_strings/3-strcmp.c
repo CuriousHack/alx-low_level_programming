4504bf933e577f31fb5e78a691e0d60c96737112
Akande Lateef <akandelateef0@gmail.com>
2023-03-02 17:36:37 -0800

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
+	int j = 0;
 
-	while (i >= 0)
+	while (j == 0)
 	{
-		if (s1[i] > s2[i])
-		{
-			return (1);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else
-		{
-			return (0);
-		}
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		j = s1[i] - s2[i];
 		i++;
 	}
+	return (j);
 }
