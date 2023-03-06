80f9bee8357a1a96f1620fffe4668e4687b62529
Akande Lateef <akandelateef0@gmail.com>
2023-03-06 15:00:20 -0800

    search character in string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function to search for a string for any byte
+ * @s:string to check from
+ * @accept: string to check
+ * Return: pointer to the byte of s
+ */
+char _strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + i))
+			break;
+		}
+		return (*(s + i));
+	}
+	return ('\0');
+}
