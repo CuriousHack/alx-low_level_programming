29cd6f2c14ef767529812001cf6c6df0943b6970
Akande Lateef <akandelateef0@gmail.com>
2023-03-06 14:19:39 -0800

    string buffer count

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,15 +10,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; *(s + i); i++)
+	for (i = 0; *(s + i) !='\0'; i++)
 	{
-		for (j = 0; *(accept + j); j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
+			{
 				break;
+			}
 		}
-		if (!*(accept + j)
-				return (i);
-	}
+		if (!*(accept + j))
+		{
+			return (i);
+		}
+			
 	return (i);
 }
