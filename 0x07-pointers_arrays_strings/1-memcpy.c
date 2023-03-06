902b80a9c3aeaec2eeeb032a6a0a120093ba9e21
Akande Lateef <akandelateef0@gmail.com>
2023-03-06 04:25:25 -0800

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - function tocopy memory address
+ * @dest: destination of the copied mem address
+ * @src: source of memory address
+ * @n: number of addresses tobe copied
+ * Return: return the newly generated addresses.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
