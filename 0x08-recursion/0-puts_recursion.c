775a872857c98d2e7663f02ea979cf8ec3d54ef2
Akande Lateef <akandelateef0@gmail.com>
2023-03-08 14:35:43 -0800

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,10 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	int n = 0;
+	if (s[n] != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(s[n]);
+		_puts_recursion(s (n + 1));
 	}
 	else
 	{
