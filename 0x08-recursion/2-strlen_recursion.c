795db12fc21280029117dc2da101c06b807732f1
Akande Lateef <akandelateef0@gmail.com>
2023-03-08 16:14:50 -0800

    string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,10 +9,12 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s)
+	if (*s == 0)
 	{
-		i++;
-		_strlen_recursion(s + 1);
+		return (0);
 	}
-	return (i);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
