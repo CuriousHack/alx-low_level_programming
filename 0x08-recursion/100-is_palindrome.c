c9d578879d19243be7b35329c847aa96ce360fd1
Akande Lateef <akandelateef0@gmail.com>
2023-03-08 17:54:18 -0800

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function toprint length of string
+ * @s: string to count
+ * Return: return the string lenght
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comp_recursion - comparer the strings
+ * @n: smallest iterator
+ * @m: biggest iterator
+ * @s: string
+ * Return: 0 or 1
+ */
+int comp_recursion(char *s, int n, int m)
+{
+	if (*(s + n) == *(s + m))
+		if (n == m || n == m + 1)
+			return (1);
+		return (comp_recursion(s, n + 1, m - 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - check for palindrome
+ * @s: string
+ * Return: 0 or 1;
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comp_recursion(*s, 0, _strlen_recursion(s - 1)));
+}
