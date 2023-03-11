6937f75f82eeae1378ade4501f4b72045e63d560
Akande Lateef <akandelateef0@gmail.com>
2023-03-10 16:08:00 -0800

    print my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: arg vector
  * Return: always 0
  */
-int main(int argc __attribute__ ((unused)), int *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
