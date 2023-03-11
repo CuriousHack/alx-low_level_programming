6e9ba2124905eff36487aaa71b05a6b66fe768d5
Akande Lateef <akandelateef0@gmail.com>
2023-03-10 16:03:24 -0800

    print my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: always 0
+ */
+int main(int argc, int *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
