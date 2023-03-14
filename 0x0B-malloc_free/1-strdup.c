5dff19b3a6d0a9ed65c969946c720d9857d8bbb9
Akande Lateef <akandelateef0@gmail.com>
2023-03-14 15:08:11 -0700

    duplicate memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - function toreturn address of duplicated string
+ * @str: string to be duplicated
+ * Return: NULL if failed, new address if success
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+	size_t len = strlen(str) + 1;
+
+	new_str = malloc(len);
+
+	if (new_str == NULL)
+		return (NULL);
+	if (str == NULL)
+		return (NULL);
+	memcpy(str, new_str, len);
+	return (new_str);
+}
