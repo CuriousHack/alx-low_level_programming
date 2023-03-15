2b287e57f116ca7b61826bb7c7e25d10b5291726
Akande Lateef <akandelateef0@gmail.com>
2023-03-14 17:09:58 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,17 +9,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, m, n;
+	unsigned int i, j, m, n;
 	char *arr;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
+
 	for (j = 0; s2[j] != '\0'; j++)
 		;
+
 	arr = malloc((i + j + 1) * sizeof(char));
 
 	if (arr == NULL)
@@ -27,12 +31,15 @@ char *str_concat(char *s1, char *s2)
 		free(arr);
 		return (NULL);
 	}
+
 	for (m = 0; m <= i; m++)
 	{
 		arr[m] = s1[m];
 	}
 	n = j;
+
 	for (j = 0; j <= n; m++, j++)
 		arr[m] = s2[j];
+
 	return (arr);
 }
