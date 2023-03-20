d32dc3df2cc2df125894dc491e9309d23ec7d643
Akande Lateef <akandelateef0@gmail.com>
2023-03-20 16:53:20 -0700

    initializing struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - function to initialize new value for struct dog
+ * @d:struct pointer for dog
+ * @dog: structure to edit
+ * @name: name value
+ * @age: age of the dog
+ * @owner: owner value of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
