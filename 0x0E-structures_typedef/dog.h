950e3b3fc5d14486d0b54f01397f1d13a7c54a1a
Akande Lateef <akandelateef0@gmail.com>
2023-03-20 15:42:31 -0700

    dog structure added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef dog
+#define dog
+/* struct dog - structure to define dog and its attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
