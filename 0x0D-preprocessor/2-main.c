714c9a7bfeb5a683887ba39faf72e4f5b22cc959
Akande Lateef <akandelateef0@gmail.com>
2023-03-21 08:55:25 -0700

    file name macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - function to return the file name
- * Return: return nothing
+ * Return: return 0 always true
  */
 
 int main(void)
 {
 	printf("%s\n", __FILE__);
+
+	return (0);
 }
